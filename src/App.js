3155189b0f23480e718622730b6256a711e0ea4e
peladvi <99794434+peladvi@users.noreply.github.com>
2022-04-05 11:19:36 +0200

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">Hello from Dictionary 🙋‍♀️</header>
+    </div>
+  );
+}
+
+export default App;
