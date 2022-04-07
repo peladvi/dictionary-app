c7da30e55c4a158dd327e67d00c4e9849b0e3d49
peladvi <99794434+peladvi@users.noreply.github.com>
2022-04-07 15:36:58 +0200

    Included phonetics

---
+++ src/Phonetic.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function Phonetic(props) {
+  return (
+    <div className="Phonetic">
+      {props.phonetic.text}
+      <a href={props.phonetic.audio} target="_blank" rel="noreferrer">
+        {" "}
+        <i className="fa-solid fa-headphones"></i>
+      </a>
+    </div>
+  );
+}
