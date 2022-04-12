c809212bf7be1a4b8761ef7a1ce31f0275070ee0
peladvi <99794434+peladvi@users.noreply.github.com>
2022-04-12 20:28:37 +0200

    Fixed the audio bug

--- src/Phonetic.js
+++ src/Phonetic.js
@@ -3,12 +3,16 @@ import "./Phonetic.css";
 import icon from "./headphones.svg";
 
 export default function Phonetic(props) {
-  return (
-    <div className="Phonetic">
-      {props.phonetic.text}
-      <a href={props.phonetic.audio} target="_blank" rel="noreferrer">
-        <img src={icon} className="icon" alt="listen" />
-      </a>
-    </div>
-  );
+  if (props.phonetic.audio) {
+    return (
+      <div className="Phonetic">
+        {props.phonetic.text}
+        <a href={props.phonetic.audio} target="_blank" rel="noreferrer">
+          <img src={icon} className="icon" alt="listen" />
+        </a>
+      </div>
+    );
+  } else {
+    return <div className="Phonetic">{props.phonetic.text}</div>;
+  }
 }
